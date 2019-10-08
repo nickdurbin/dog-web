46cc0fb2e561e13451616cec0c2ce5cf9fb53fcb
nick.durbin <nick.durbin@gmail.com>
2019-10-07 21:40:00 -0400

    Added custom hooks for the useEffect.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,31 @@
 import "./App.css";
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import { useLocalStorage } from './utils/input';
-
+import { useDogImages } from './utils/api';
+ 
 
 function App (props) {
   const [breed, setBreed] = useLocalStorage('breed', 'husky')
-  const [images, setImages] = useState([])
-
-  useEffect(() => {
-    setImages([])
-    fetchDogImages()
-  }, [breed])
-
-  // extract this function from lifecycle event since it's used multiple times
-  const fetchDogImages = () => {
-    axios.get(`https://dog.ceo/api/breed/${breed}/images`)
-      .then(result => {
-        setImages(result.data.message)
-      })
-      .catch(error => {
-        console.log('error:', error)
-      })
-  }
-
-  // make our select field controlled by react state
-  const handleChange = (event) => {
-    setBreed(event.target.value)
-  }
+  const [count, setCount] = useLocalStorage('count', 1)
+  const [images] = useDogImages(breed, count)
 
   return (
     <>
       <h1>The Dog Website</h1>
 
-      <select value={breed} onChange={handleChange}>
+      <select value={breed} onChange={e => setBreed(e.target.value)}>
         <option value="husky">Husky</option>
         <option value="beagle">Beagle</option>
         <option value="corgi">Corgi</option>
         <option value="boxer">Boxer</option>
       </select>
+
+      <input 
+        type='number'
+        placeholder='Image Count'
+        value={count}
+        onChange={e => setCount(e.target.value)}
+      />
 
       <div>
         {images.map((image, index) => (
